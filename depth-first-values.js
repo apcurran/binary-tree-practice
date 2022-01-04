18afdd0fcd0d04de3c308bb6fb5cc549d6a915f3
Alex Curran <alexcurran.va@gmail.com>
2022-01-04 15:50:11 -0600

    Connect nodes for setup

--- depth-first-values.js
+++ depth-first-values.js
@@ -1,1 +1,32 @@
 "use strict";
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new ListNode("a");
+const b = new ListNode("b");
+const c = new ListNode("c");
+const d = new ListNode("d");
+const e = new ListNode("e");
+const f = new ListNode("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+function depthFirstValues(root) {
+
+}
